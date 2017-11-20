c75cd8ea6b1f7801c084a4382f0f9ac35b8fc455
Tayrih <tahysluana@gmail.com>
2017-11-19 23:26:22 -0500

    version 0,0,6

--- js/app.js
+++ js/app.js
@@ -32,83 +32,65 @@ function createInput(event) {
 
   contentGeneral.appendChild(contentForm);
 
-  var butonSave = document.getElementById('new-buton'); // variable para obtener el boton inicial: GUARDAR
+
   // evento click para guardar el texto ingresado
-  butonSave.addEventListener('click', newTareaBox);
-}
+  buton.addEventListener('click', function () {
+    if(newInput.value) {
+        var contentTextList = document.createElement('div'); // contenedor de las nuevas tareas
+        var pValue = document.createElement('p');
+        var pNewTarea = document.createElement('p');
 
-function newTareaBox(event) {
-  var form = document.getElementById('first-form'); //primer div que almacena el formulario para agregar tareas
-  var newInputBox = document.getElementById('new-input');
+        contentTextList.setAttribute('class', 'new-form left');
+        pValue.setAttribute('class', 'contenido');
+        pValue.setAttribute('id', 'title');
+        pNewTarea.setAttribute('class', 'add-homework');
 
-  if(newInputBox.value) {
-      var contentTextList = document.createElement('div'); // contenedor de las nuevas tareas
-      var pValue = document.createElement('p');
-      var pNewTarea = document.createElement('p');
+        pNewTarea.textContent = 'Añadir tarea';
+        pValue.textContent = newInput.value;
 
-      contentTextList.setAttribute('class', 'new-form left');
-      pValue.setAttribute('class', 'contenido');
-      pValue.setAttribute('id', 'title');
-      pNewTarea.setAttribute('class', 'add-homework');
+        contentTextList.appendChild(pValue);
+        contentTextList.appendChild(pNewTarea);
+        contentGeneral.appendChild(contentTextList);
+        contentGeneral.insertBefore(contentTextList,referencia);
 
-      pNewTarea.textContent = 'Añadir tarea';
-      pValue.textContent = newInputBox.value;
+        newInput.value = '';
 
-      contentTextList.appendChild(pValue);
-      contentTextList.appendChild(pNewTarea);
-      contentGeneral.appendChild(contentTextList);
-      contentGeneral.insertBefore(contentTextList,referencia);
+        pNewTarea.addEventListener('click', function (event) {
+          // contentTareas.replaceChild(nuevo_nodo,newTarea);
+        contentTextList.removeChild(pNewTarea);
 
-      newInputBox.value = '';
+        var contentForm = document.createElement('div');
+        var newInput = document.createElement('textarea');
+        var buton = document.createElement('button');
+        var spanIcon = document.createElement('span');
 
-      var contentTareas = document.getElementsByClassName('new-form left')[0];  // selecciona a los contenedores de tareas con ese nombre de clase
-      var newTarea = document.getElementsByClassName('add-homework')[0]; // añadir tarea...
-      newTarea.addEventListener('click', function (event) {
-        // contentTareas.replaceChild(nuevo_nodo,newTarea);
-        contentTareas.removeChild(newTarea);
-        createNewCard(event);
-      });
-  }
-}
+        contentForm.setAttribute('class', 'new-form card');
+        newInput.setAttribute('id', 'new-input-card');
+        newInput.setAttribute('placeholder', 'Añadir una tarea...');
+        buton.setAttribute('id', 'new-buton-card');
 
+        buton.textContent = "Añadir";
+        spanIcon.innerHTML = '<i class="fa fa-times" aria-hidden="true"></i>';
 
-function createNewCard() {
-  var contentTareas = document.getElementsByClassName('new-form left')[0];
+        contentTextList.appendChild(contentForm);
+        contentForm.appendChild(newInput);
+        newInput.focus();
+        contentForm.appendChild(buton);
+        contentForm.appendChild(spanIcon);
 
-  var contentForm = document.createElement('div');
-  var newInput = document.createElement('textarea');
-  var buton = document.createElement('button');
-  var spanIcon = document.createElement('span');
-
-  contentForm.setAttribute('class', 'new-form card');
-  newInput.setAttribute('id', 'new-input-card');
-  newInput.setAttribute('placeholder', 'Añadir una tarea...');
-  buton.setAttribute('id', 'new-buton-card');
-
-  buton.textContent = "Añadir";
-  spanIcon.innerHTML = '<i class="fa fa-times" aria-hidden="true"></i>';
-
-  contentTareas.appendChild(contentForm);
-  contentForm.appendChild(newInput);
-  newInput.focus();
-  contentForm.appendChild(buton);
-  contentForm.appendChild(spanIcon);
-
-
-  var butonSave = document.getElementById('new-buton-card'); // boton para añadir una lista de tareas
-  // evento click para guardar el texto ingresado
-  butonSave.addEventListener('click', function () {
-    var form = document.getElementsByClassName('new-form card')[0];
-    var newInputBox = document.getElementById('new-input-card');
-    var parentTitle = document.getElementById('title').parentNode;
-    console.log('Te odio js');
-   if(newInputBox.value) {
-        var pCard = document.createElement('p');
-        pCard.setAttribute('class', 'content-list');
-        pCard.textContent = newInputBox.value;
-        parentTitle.insertBefore(pCard,form);
-        pCard.focus();
-        newInputBox.value = '';
-      }
-    });
+        // evento click para guardar el texto ingresado
+        buton.addEventListener('click', function () {
+          console.log('Te odio js');
+         if(newInput.value) {
+              var pCard = document.createElement('p');
+              pCard.setAttribute('class', 'content-list');
+              pCard.textContent = newInput.value;
+              contentTextList.insertBefore(pCard,contentForm);
+              pCard.focus();
+              newInput.value = '';
+            }
+          });
+        });
+    }
+  });
 }
